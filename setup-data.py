15ac86f3c1b72b8872dbc19f0d4bc7e78f1d1303
Chris Evans <chrisevans9629@gmail.com>
2021-12-08 18:52:20 -0600

    converted total cases to daily cases

--- setup-data.py
+++ setup-data.py
@@ -8,23 +8,64 @@ import pandas as pd
 # Read the actual cases and convert the data from wide format to row format
 
 # %%
-data = pd.read_csv('covid_confirmed_usafacts.csv')
 
-data = data.melt(id_vars=["countyFIPS","County Name", "State", "StateFIPS"], var_name="date", value_name="value")
+try:
+    files = []
+    for i in range(2):
+        files.append('confirmed_daily_cases{id}.csv'.format(id=i))
+    data = pd.concat(map(pd.read_csv, files), ignore_index=True)
+    print('found files for actual daily cases')
+    print(data.head(4))
+    confirmed_cases = data
+except FileNotFoundError:
+    print('could not find daily cases files.  regenerating...')
+    data = pd.read_csv('covid_confirmed_usafacts.csv')
+    # convert column wide to rows
+    data = data.melt(id_vars=["countyFIPS","County Name", "State", "StateFIPS"], var_name="date", value_name="value")
+    print(data.head(4))
 
-data.head(4)
+    # select columns
+    data = data[['countyFIPS','value','date', 'County Name', 'State']]
 
+    data['date'] = pd.to_datetime(data['date'])
 
-# %%
-data = data[['countyFIPS','value','date', 'County Name', 'State']]
+    # rename columns
+    data = data.rename(columns={"countyFIPS": "fips", "value": "act_cases", "County Name": "county", "State": "state"})
 
-data['date'] = pd.to_datetime(data['date'])
+    print(data.head(10))
 
-data = data.rename(columns={"countyFIPS": "fips", "value": "act_cases", "County Name": "county", "State": "state"})
+    # gets the previous row value
+    def getDaily(values):
+        daily = []
+        length = len(values)
+        for i in range(length):
+            if i == 0:
+                daily.append(values[i])
+            else:
+                daily.append(values[i]-values[i-1])
+        return daily
 
-confirmed_cases = data
+    # get the counties
+    counties = data.groupby('fips')
 
-data.head(10)
+    dataset = pd.DataFrame()
+
+    # for each county, sort by date and get the daily cases for each county
+    for key, county in counties:
+        county = county.sort_values(by='date').reset_index()
+        act_cases = county['act_cases']
+        county['daily_cases'] = getDaily(act_cases)
+        dataset = dataset.append(county)
+
+    dataset['act_cases'] = dataset['daily_cases']
+
+    dataset = dataset.drop(['daily_cases','index'], 1)
+
+    confirmed_cases = dataset
+    # write the cases to a file
+    import numpy as np
+    for id, df_i in enumerate(np.array_split(confirmed_cases, 2)):
+        df_i.to_csv('confirmed_daily_cases{id}.csv'.format(id=id), index=False)
 
 # %% [markdown]
 # # Step 2
@@ -67,13 +108,6 @@ pop_data = pop_data.rename(columns={'countyFIPS': "fips"})
 pop_data.head(10)
 
 # %% [markdown]
-# # Step 2.2
-# Verify Data integrity
-
-# %%
-
-
-# %% [markdown]
 # # Step 3
 # Merge the two datasets together based on predication target date and the county
 
@@ -104,5 +138,19 @@ all_cases.head(10)
 
 # %%
 all_cases.to_csv('all_cases.csv')
+
+# %% [markdown]
+# # Data Validation Test
+
+# %%
+all_cases[(all_cases.state == "MO") & (all_cases.county == "Jackson County ") & (all_cases.model == "Columbia") & (all_cases.target == "1 wk ahead inc case")].sort_values(by='date')
+
+
+# %%
+all_cases[(all_cases.state == "AK") & (all_cases.county == "Aleutians East Borough ") & (all_cases.model == "Columbia") & (all_cases.target == "1 wk ahead inc case")].sort_values(by='date')
+
+
+# %%
+all_cases[(all_cases.for_cases < 0)]
 
 
